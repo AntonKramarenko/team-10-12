459e4e6f17d5323d9afb3fb487e68100794cfa36
Anton Kranarenko <a.kramarenko@evopay.com.ua>
2023-12-09 15:48:52 +0200

    some fix

--- routes/mailing.js
+++ routes/mailing.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 const Mailing = require('../models/mailing')
+const Entity = require("../models/entity");
 
 
 router.get('/', async (req, res) => {
@@ -19,6 +20,19 @@ router.post('/', async (req, res) => {
     const entity = new Mailing({...req.body})
     await entity.save()
     return res.sendStatus(200)
+  } catch (error){
+    console.log('error', error);
+    res.statusMessage = "GET error";
+    res.status(400).end();
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Mailing.findOneAndDelete({ id: req.params.id })
+    const entities = await Mailing.find()
+
+    res.json(entities)
   } catch (error){
     console.log('error', error);
     res.statusMessage = "GET error";
